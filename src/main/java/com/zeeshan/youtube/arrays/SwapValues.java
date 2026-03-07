858670730e06fa5362c19a8823d4e5dde56fd567
Zeeshan Ahmad <azeeshan867@gmail.com>
2026-03-07 13:15:24 +0530

    Add SwapValues class use to swap values based on indexes.

---
+++ src/main/java/com/zeeshan/youtube/arrays/SwapValues.java
@@ -0,0 +1,16 @@
+package com.zeeshan.youtube.arrays;
+
+public class SwapValues {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        swap(arr,2,4);
+        for(int i:arr){
+            System.out.print(i + " ");
+        }
+    }
+    public static void swap(int[] arr,int index1,int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}

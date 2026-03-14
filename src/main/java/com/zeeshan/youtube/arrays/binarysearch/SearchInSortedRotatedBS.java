0c221a7b57272504f32c77a5b1d67f50d0631649
Zeeshan Ahmad <azeeshan867@gmail.com>
2026-03-14 08:30:21 +0530

    Create SearchInSortedRotatedBS.java

---
+++ src/main/java/com/zeeshan/youtube/arrays/binarysearch/SearchInSortedRotatedBS.java
@@ -0,0 +1,55 @@
+package com.zeeshan.youtube.arrays.binarysearch;
+
+public class SearchInSortedRotatedBS {
+    // https://leetcode.com/problems/search-in-rotated-sorted-array/
+    public static void main(String[] args) {
+        int arr[] = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr,target));
+    }
+    public static int search(int arr[], int target){
+        int pivot = findPivot(arr);
+        System.out.println("Pivot Index: "+ pivot);
+        if(pivot==-1){
+            return binarySearch(arr,target,0,arr.length-1);
+        }
+        if(arr[pivot]==target){
+            return pivot;
+        }
+        if(target>=arr[0]){
+            return binarySearch(arr,target,0,pivot-1);
+        }
+        return binarySearch(arr,target,pivot+1,arr.length-1);
+    }
+    public static int binarySearch(int[] arr,int target, int start, int end){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] == target){
+                return mid;
+            } else if(arr[mid]<target){
+                start = mid+1;
+            } else if(arr[mid]>target){
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static int findPivot(int arr[]){
+        int start =0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid> start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }if(arr[start]>= arr[mid]){
+                end = mid-1;
+            } else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+}

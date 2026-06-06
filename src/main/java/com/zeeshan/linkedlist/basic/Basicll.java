3aa40fbbf2a589577b357725d9f719f5596a7af5
Zeeshan Ahmad <azeeshan867@gmail.com>
2026-06-06 11:55:30 +0530

    Basic Singly Linked List example

---
+++ src/main/java/com/zeeshan/linkedlist/basic/Basicll.java
@@ -0,0 +1,25 @@
+package com.zeeshan.linkedlist.basic;
+
+public class Basicll {
+    public static void main(String[] args) {
+        Node node1 = new Node(5);
+        Node node2 = new Node(8);
+        Node node3 = new Node(3);
+        Node node4 = new Node(9);
+        node1.next =  node2;
+        node2.next =  node3;
+        node3.next =  node4;
+        System.out.println(node1.data);
+        System.out.println(node1.next.data);
+        System.out.println(node1.next.next.data);
+        System.out.println(node1.next.next.next.data);
+
+    }
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+}

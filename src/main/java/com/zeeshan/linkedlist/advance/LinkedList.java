1b1903910546b79adb1bf60d9c453869fc0b9344
Zeeshan Ahmad <azeeshan867@gmail.com>
2026-06-07 12:47:39 +0530

    Add simple singly-linked list and demo Main
    Introduce LinkedList (com.zeeshan.linkedlist.advance) implementing a basic singly-linked list with an inner Node class, head/tail pointers, and methods: insertAtEnd(int), size(), and display(). Add Main.java to demonstrate usage by inserting sample values (10, 4, 90, 20), printing the list size and contents.

---
+++ src/main/java/com/zeeshan/linkedlist/advance/LinkedList.java
@@ -0,0 +1,43 @@
+package com.zeeshan.linkedlist.advance;
+
+public class LinkedList {
+
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    Node head = null;
+    Node tail = null;
+
+    void insertAtEnd(int val){
+        Node temp = new Node(val);
+        if(head==null){
+            head = temp;
+        }else{
+            tail.next = temp;
+        }
+        tail = temp;
+    }
+
+    public int size(){
+        Node temp = head;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
---
+++ src/main/java/com/zeeshan/linkedlist/advance/Main.java
@@ -0,0 +1,13 @@
+package com.zeeshan.linkedlist.advance;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtEnd(10);
+        list.insertAtEnd(4);
+        list.insertAtEnd(90);
+        list.insertAtEnd(20);
+        System.out.println("Size of list is: " + list.size());
+        list.display();
+    }
+}
